cb07b633b1a4f0c6306cc2f9f4c0837ee13924e7
luo2 <506765498@qq.com>
2024-04-08 21:34:39 +0800

    use IdenticonAvatar in home header

--- src/compoments/home_header.js
+++ src/compoments/home_header.js
@@ -5,97 +5,108 @@ import ConnectedSitesIcon from "data-base64:~assets/icons/connected_sites.png"
 import LockUtilityIcon from "data-base64:~assets/icons/lock_utility.png"
 import SettingsIcon from "data-base64:~assets/icons/settings.png"
 import SupportIcon from "data-base64:~assets/icons/support.png"
-import DevAvatar from "data-base64:~assets/images/dev_avatar.png"
-import React from "react"
+import React, {useEffect, useState} from "react"
 // import ConnectToMobileWalletIcon from "data-base64:~assets/icons/connect_to_mobile_wallet.png";
-import { useNavigate } from "react-router-dom"
-
+import {useNavigate} from "react-router-dom"
+import IdenticonAvatar from "~utils/identiconAvatar"
 import Dropdown from "./menu_dropdown"
+import TopStorage from "~db/user_storage";
 
 const HomeHeader = ({
-  _handleAccountDetailsClick,
-  _handleCoonectedSitesClick,
-  _handleConnectToMobileWalletClick
-}) => {
-  const navigate = useNavigate()
+                        _handleAccountDetailsClick,
+                        _handleCoonectedSitesClick,
+                        _handleConnectToMobileWalletClick
+                    }) => {
+    const navigate = useNavigate()
 
-  const handleAccountDetailsClick = () => {
-    _handleAccountDetailsClick()
-  }
-
-  const handleCoonectedSitesClick = () => {
-    _handleCoonectedSitesClick()
-  }
-
-  // 需替换为帮助网页
-  const handleSupportClick = () => {
-    const blankPageUrl = "https://utlab.io/"
-
-    // 在新标签页中打开空白页面
-    window.open(blankPageUrl, "_blank")
-  }
-
-  const handleSettingsClick = () => {
-    navigate("/tabs/home.html#settings")
-  }
-
-  const handleLockUtilityClick = () => {}
-
-  // const handleConnectToMobileWalletClick = () => {
-  //     _handleConnectToMobileWalletClick();
-  // }
-
-  const options = [
-    {
-      value: "/tabs/home.html#account_details",
-      label: "account_details",
-      icon: AccountDetailsIcon,
-      handleClick: handleAccountDetailsClick
-    },
-    {
-      value: "/tabs/home.html#connected_sites",
-      label: "connected_sites",
-      icon: ConnectedSitesIcon,
-      handleClick: handleCoonectedSitesClick
-    },
-    {
-      value: "/tabs/home.html#support",
-      label: "support",
-      icon: SupportIcon,
-      handleClick: handleSupportClick
-    },
-    {
-      value: "/tabs/home.html#settings",
-      label: "settings",
-      icon: SettingsIcon,
-      handleClick: handleSettingsClick
-    },
-    {
-      value: "/tabs/home.html#lock_utility",
-      label: "lock_utility",
-      icon: LockUtilityIcon,
-      handleClick: handleLockUtilityClick
+    const handleAccountDetailsClick = () => {
+        _handleAccountDetailsClick()
     }
-    // { value: '/tabs/home.html#connect_to_mobile_wallet', label: 'connect_to_mobile_wallet', icon: ConnectToMobileWalletIcon, handleClick: handleConnectToMobileWalletClick },
-  ]
 
-  return (
-    <div className="home_header">
-      <div style={{ width: "24px", height: "24px" }}></div>
-      <div className="flex_center_center text_black_14_600">
-        <img
-          src={DevAvatar}
-          style={{ width: "30px", height: "30px", margin: "5px" }}
-          alt={"Avatar"}
-        />
-        Account 1
-      </div>
-      <Dropdown options={options} />
-      {/* <Link to="/tabs/home.html#settings">
+    const [currentAddress, setCurrentAddress] = useState(
+        "0x010101011010101001010101101010100101010110101010"
+    )
+
+    useEffect(() => {
+        const checkCurrentAddress = async () => {
+            const myAddress = await TopStorage.getMyAddress()
+            if (myAddress !== undefined) {
+                setCurrentAddress(myAddress)
+            }
+        }
+        checkCurrentAddress()
+    }, [])
+
+    const handleCoonectedSitesClick = () => {
+        _handleCoonectedSitesClick()
+    }
+
+    // 需替换为帮助网页
+    const handleSupportClick = () => {
+        const blankPageUrl = "https://utlab.io/"
+
+        // 在新标签页中打开空白页面
+        window.open(blankPageUrl, "_blank")
+    }
+
+    const handleSettingsClick = () => {
+        navigate("/tabs/home.html#settings")
+    }
+
+    const handleLockUtilityClick = () => {
+    }
+
+    // const handleConnectToMobileWalletClick = () => {
+    //     _handleConnectToMobileWalletClick();
+    // }
+
+    const options = [
+        {
+            value: "/tabs/home.html#account_details",
+            label: "account_details",
+            icon: AccountDetailsIcon,
+            handleClick: handleAccountDetailsClick
+        },
+        {
+            value: "/tabs/home.html#connected_sites",
+            label: "connected_sites",
+            icon: ConnectedSitesIcon,
+            handleClick: handleCoonectedSitesClick
+        },
+        {
+            value: "/tabs/home.html#support",
+            label: "support",
+            icon: SupportIcon,
+            handleClick: handleSupportClick
+        },
+        {
+            value: "/tabs/home.html#settings",
+            label: "settings",
+            icon: SettingsIcon,
+            handleClick: handleSettingsClick
+        },
+        {
+            value: "/tabs/home.html#lock_utility",
+            label: "lock_utility",
+            icon: LockUtilityIcon,
+            handleClick: handleLockUtilityClick
+        }
+        // { value: '/tabs/home.html#connect_to_mobile_wallet', label: 'connect_to_mobile_wallet', icon: ConnectToMobileWalletIcon, handleClick: handleConnectToMobileWalletClick },
+    ]
+
+    return (
+        <div className="home_header">
+            <div style={{width: "24px", height: "24px"}}></div>
+            <div className="flex_center_center text_black_14_600">
+                <IdenticonAvatar address={currentAddress} size={30}></IdenticonAvatar>
+                <div style={{marginLeft: "5px"}}>Account 1</div>
+            </div>
+            <Dropdown options={options}/>
+            {/* <Link to="/tabs/home.html#settings">
                 <img src={menuImg} style={{ width: "24px", height: "24px" }} onClick={handleMenuClick}/>
             </Link> */}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default HomeHeader
